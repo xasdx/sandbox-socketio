9fb332923c43ddb05a8d1a63b3bffc0f2e8142d5
_asdx_ <xasdx@tuta.io>
2018-02-15 10:47:40 +0000

    Implement a simple multiclient chat application

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ let app = require("express")()
 let server = require("http").Server(app)
 let io = require("socket.io")(server)
 
-server.listen(3210)
+server.listen(process.env.WEB_APP_PORT)
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"))
 
 io.on("connection", (socket) => {
   console.log("client connected")
-  socket.emit("test", { name: "john" })
-  socket.on("test-event", console.log)
+  socket.on("new_client_msg", (msg) => {
+    console.log(`${msg.name} commented ${msg.message}`)
+    socket.broadcast.emit("new_msg", msg)
+    socket.emit("new_msg", msg)
+  })
 })
